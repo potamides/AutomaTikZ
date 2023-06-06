b72220123e7bbdedabf9ea41ad0f1fb238f62e67
Jonas Belouadi <53401822+potamides@users.noreply.github.com>
2023-06-06 10:33:23 +0200

    feat: update model inference pipeline and code

--- examples/infer.py
+++ examples/infer.py
@@ -1,52 +1,98 @@
 #!/usr/bin/env -S python -i
 from argparse import ArgumentParser
-from os import listdir
-from os.path import isdir, join
+from os.path import isfile
+from sys import flags
 
+from PIL import UnidentifiedImageError
+from datasets import DownloadManager as DL
 from peft import PeftModel
-from transformers import AutoConfig, AutoModelForSeq2SeqLM, AutoTokenizer, PretrainedConfig
-from transformers.utils.logging import enable_explicit_format, set_verbosity_info
+import torch
+from transformers import (
+    AutoConfig,
+    AutoModelForCausalLM,
+    AutoModelForSeq2SeqLM,
+    AutoTokenizer,
+    PretrainedConfig,
+    set_seed,
+)
+from transformers.utils.hub import get_file_from_repo, is_remote_url
 
 from scidraw import train
 from scidraw.infer import TikZGenerator
-from scidraw.infer.llama import merge_and_unload
+from scidraw.model.clima import register
+from scidraw.util import merge_and_unload
+
+try:
+    import readline # patches input()
+except:
+    pass #readline not available
 
 def load(path):
-    try:
-        model = AutoModelForSeq2SeqLM.from_pretrained(path)
-        tokenizer = AutoTokenizer.from_pretrained(path)
-        return model, tokenizer
-    except EnvironmentError:
-        pass
+    register()
+    for AutoModel in [AutoModelForSeq2SeqLM, AutoModelForCausalLM]:
+        try:
+            model = AutoModel.from_pretrained(path)
+            tokenizer = AutoTokenizer.from_pretrained(path)
+            return model, tokenizer
+        except EnvironmentError:
+            pass
 
-    if isdir(path) and (conf := "adapter_config.json") in listdir(path):
-        base_model = PretrainedConfig.get_config_dict(join(path, conf))[0]["base_model_name_or_path"]
-        model_type = AutoConfig.from_pretrained(base_model).model_type
+    if (is_remote:=is_remote_url(path)) or isfile(path): # treat it as a pretrained mm projector
+        hidden_size = len(torch.load(DL().download(path) if is_remote else path)['model.mm_projector.weight'])
+        size_dict = {
+            4096: "7b",
+            5120: "13b",
+            6656: "30b",
+            8192: "65b"
+        }
+        model, tokenizer = train.clima.load(pretrain_mm_mlp_adapter=path, size=size_dict[hidden_size])
+    elif conf_file := get_file_from_repo(path, "adapter_config.json"): # local folder or on huggingface hub
+        conf = PretrainedConfig.get_config_dict(conf_file)[0]
+        base_model = conf["base_model_name_or_path"]
+        model_type = conf.get("model_type", AutoConfig.from_pretrained(base_model).model_type)
         model, tokenizer = getattr(train, model_type).load(base_model=base_model)
         model = merge_and_unload(PeftModel.from_pretrained(
             model,
             path,
             torch_dtype=model.config.torch_dtype,
-        )).eval()
-
-        return model, tokenizer
+        ))
     else:
         raise ValueError(f"Cannot load model from {path}.")
 
+    return model.eval(), tokenizer
 
 def parse_args():
     argument_parser = ArgumentParser(
-        description="Inference helper for fine-tuned models (best run with python -i)"
+        description="Inference helper for fine-tuned models."
     )
     argument_parser.add_argument(
         "--path",
         required=True,
-        help="directory where to find models/adapters (local or hub)",
+        help="path or url to projector weights or directory where to find models/adapters (local or hub)",
     )
     return argument_parser.parse_args()
 
 if __name__ == "__main__":
-    set_verbosity_info()
-    enable_explicit_format()
-
+    set_seed(0)
     generate = TikZGenerator(*load(parse_args().path))
+    desc = dict(
+        caption="the caption",
+        image="optional input fed into CLIP, defaults to the caption (can be a Pillow Image, a URI to an image, or a caption)"
+    )
+
+    if flags.interactive:
+        print("generate(*args, **kwargs):", str(TikZGenerator.generate.__doc__).strip())
+    else:
+        print("Starting a REPL for generating TikZ. Arguments:", *[f"\t{k}: {v}" for k, v in desc.items()], sep="\n")
+        while True:
+            try:
+                caption = input("Caption: ")
+                image = input("Image (optional): ") if generate.processor else None
+            except (KeyboardInterrupt, EOFError):
+                break
+            try:
+                generate(caption=caption, image=image or None)
+            except KeyboardInterrupt:
+                pass
+            except UnidentifiedImageError:
+                print("Error: Cannot identify image file!")
--- scidraw/infer/__init__.py
+++ scidraw/infer/__init__.py
@@ -1,3 +1,9 @@
+from os.path import isfile
+from typing import Optional, Union
+import warnings
+
+from PIL import Image, ImageOps
+import requests
 import torch
 from torch.cuda import current_device, is_available as has_cuda
 from transformers import (
@@ -5,28 +11,38 @@ from transformers import (
     PhrasalConstraint,
     Text2TextGenerationPipeline as T2TGP,
     TextGenerationPipeline as TGP,
+    TextStreamer,
 )
+from transformers.utils import logging
+from transformers.utils.hub import is_remote_url
+
+logger = logging.get_logger("transformers")
 
 class TikZGenerator:
     def __init__(
         self,
         model,
         tokenizer,
-        temperature=0.2,
+        temperature=0.2, # generation parameters based on results from codeparrot
+        top_p=0.95,
         max_length=1024,
         num_beams=1,
+        stream=True,
+        expand_to_square=False,
     ):
         self.enc_dec = model.config.is_encoder_decoder
+        self.expand_to_square = expand_to_square
+        self.processor = getattr(tokenizer, "image", None)
         self.pipeline = (T2TGP if self.enc_dec else TGP)(
             model=model,
-            tokenizer=tokenizer,
+            tokenizer=getattr(tokenizer, "text", tokenizer),
             device=current_device() if has_cuda() else -1,
         )
-        self.pipeline.model = torch.compile(model)
+        self.pipeline.model = torch.compile(model) # pyright: ignore
 
-        self.gen_kwargs = dict(
+        self.default_kwargs = dict(
             temperature=temperature,
-            # top_p=0.9,
+            top_p=top_p,
             num_beams=num_beams,
             #constraints=self._prepare_constraints(),
             num_return_sequences=1,
@@ -34,7 +50,18 @@ class TikZGenerator:
             do_sample=True,
             clean_up_tokenization_spaces=True,
             #remove_invalid_values=True,
-        ) | ({} if self.enc_dec else dict(return_full_text=False))
+            return_full_text=False,
+            streamer=TextStreamer(self.pipeline.tokenizer, # pyright: ignore
+                skip_prompt=True,
+                clean_up_tokenization_spaces=True,
+                skip_special_tokens=True
+            ),
+        )
+
+        if self.enc_dec:
+            self.default_kwargs.pop("return_full_text")
+        if not stream:
+            self.default_kwargs.pop("streamer")
 
     def _prepare_constraints(self):
         with_prefix_space = [
@@ -57,12 +84,45 @@ class TikZGenerator:
         )
 
 
-    def generate(self, instruction):
-        tokenizer = self.pipeline.tokenizer
-        return self.pipeline(
-            instruction + ("" if self.enc_dec else tokenizer.sep_token),
-            **self.gen_kwargs,
-        )[0]["generated_text"].strip()
+    def generate(self, caption: str, image: Optional[Union[Image.Image, str]]=None, **gen_kwargs):
+        """
+        Generate TikZ for a given caption.
+            caption: the caption
+            image: optional input fed into CLIP, defaults to the caption (can be a Pillow Image, a URI to an image, or a caption)
+            gen_kwargs: additional generation kwargs (potentially overriding the default ones)
+        """
+        model, tokenizer = self.pipeline.model, self.pipeline.tokenizer
+
+        if self.processor:
+            if isinstance(image, str) and (isfile(image) or is_remote_url(image)):
+                image = Image.open(image if isfile(image) else requests.get(image, stream=True).raw)
+            if isinstance(image, Image.Image):
+                if self.expand_to_square:
+                    image = ImageOps.pad(image, 2 * [max(image.size)], color='white') # pyright: ignore
+                img_inputs = self.processor(images=image, return_tensors="pt").to(model.device)
+                img_inputs = {k: v.type(model.dtype) for k, v in img_inputs.items()}
+            else: # make use of multi-modality of clip and encode text as image
+                logger.info("Visual information provided as text, using text model from CLIP.")
+                img_inputs = self.processor(text=image or caption, return_tensors="pt", truncation=True).to(model.device)
+
+            num_patches = model.get_model().vision_tower[0].config.num_patches # pyright: ignore
+            caption = num_patches * tokenizer.mask_token + caption # pyright: ignore
+            gen_kwargs = gen_kwargs | dict(images=img_inputs)
+
+        if tokenizer.add_bos_token: # pyright: ignore
+            # unexpectedly pipeline doesn't do this automatically...
+            caption = tokenizer.bos_token + caption # pyright: ignore
+        if not self.enc_dec:
+            # we add a sep token for decoder only models
+            caption = caption + tokenizer.sep_token # pyright: ignore
+
+        # Suppress warning about using the pipeline sequentially (we are doing that on purpose!)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            return self.pipeline( # pyright: ignore
+                caption,
+                **(self.default_kwargs | gen_kwargs),
+            )[0]["generated_text"].strip() # pyright: ignore
 
     def __call__(self, *args, **kwargs):
         return self.generate(*args, **kwargs)
