ae5ab5484fea35cfe9efdf179c05a5057645ea8a
Jonas Belouadi <53401822+potamides@users.noreply.github.com>
2023-07-03 14:35:15 +0200

    fix: repeated registering could fail

--- scidraw/model/clima/__init__.py
+++ scidraw/model/clima/__init__.py
@@ -3,5 +3,8 @@ from transformers import AutoConfig, AutoModelForCausalLM
 from .modeling_clima import ClimaConfig, ClimaForCausalLM
 
 def register():
-    AutoConfig.register("clima", ClimaConfig)
-    AutoModelForCausalLM.register(ClimaConfig, ClimaForCausalLM)
+    try:
+        AutoConfig.register("clima", ClimaConfig)
+        AutoModelForCausalLM.register(ClimaConfig, ClimaForCausalLM)
+    except ValueError:
+        pass # already registered
