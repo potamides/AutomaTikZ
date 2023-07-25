0bbbea36a7ca93d7da624f7412c1b3e8a1cf3a2f
Jonas Belouadi <53401822+potamides@users.noreply.github.com>
2023-07-25 10:10:45 +0200

    refactor: make metrics dependencies optional

--- scivg/evaluate/__init__.py
+++ scivg/evaluate/__init__.py
@@ -1,7 +1,17 @@
-from evaluate import load as _load
 from os.path import dirname, isdir, join
 
+from ..util import optional_dependencies
+
+with optional_dependencies():
+    from evaluate import load as _load
+
 def load(path, *args, **kwargs):
-    if isdir(local := join(dirname(__file__), path)):
-        return _load(local, *args, **kwargs)
-    return _load(path, *args, **kwargs)
+    try:
+        if isdir(local := join(dirname(__file__), path)):
+            return _load(local, *args, **kwargs)
+        return _load(path, *args, **kwargs)
+    except (NameError, ImportError):
+        raise ValueError(
+            "Missing dependencies: "
+            "Install this project with the [evaluate] feature name!"
+        )
