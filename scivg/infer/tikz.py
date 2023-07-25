126ea86519c2c78475e8739180e522ac07a73ecd
Jonas Belouadi <53401822+potamides@users.noreply.github.com>
2023-07-25 10:21:22 +0200

    feat: improve tikz/latex wrapper

--- scivg/infer/tikz.py
+++ scivg/infer/tikz.py
@@ -1,8 +1,10 @@
 from collections import namedtuple
-from functools import cache
+from functools import cache, cached_property
 from io import BytesIO
-from os.path import isfile
-from subprocess import DEVNULL, TimeoutExpired, run
+from os import environ
+from os.path import isfile, join
+from re import MULTILINE, escape, search, sub
+from subprocess import CalledProcessError, DEVNULL, TimeoutExpired, check_output
 from tempfile import NamedTemporaryFile, TemporaryDirectory
 from typing import Optional, Union
 import warnings
@@ -11,11 +13,7 @@ from PIL import Image, ImageOps
 import requests
 import torch
 from torch.cuda import current_device, is_available as has_cuda
-from transformers import (
-    Text2TextGenerationPipeline as T2TGP,
-    TextGenerationPipeline as TGP,
-    TextStreamer,
-)
+from transformers import TextGenerationPipeline as TGP, TextStreamer
 from transformers.utils import logging
 from transformers.utils.hub import is_remote_url
 
@@ -24,12 +22,18 @@ from ..util import optional_dependencies
 with optional_dependencies():
     from pdf2image.pdf2image import convert_from_bytes
     from pdfCropMargins import crop
+    import fitz
 
 logger = logging.get_logger("transformers")
 
 class PdfDocument:
     def __init__(self, raw: bytes):
         self.raw = raw
+
+    def save(self, filename):
+        with open(filename, "wb") as f:
+            f.write(self.raw)
+
 
 class TikzDocument:
     """
@@ -39,8 +43,8 @@ class TikzDocument:
     optional dependencies).
     """
      # engines to try, could also try: https://tex.stackexchange.com/a/495999
-    engines = ["lualatex", "pdflatex", "xelatex"]
-    Output = namedtuple("Output", ['pdf', 'status'], defaults=[None, -1])
+    engines = ["pdflatex", "lualatex", "xelatex"]
+    Output = namedtuple("Output", ['pdf', 'status', 'log'], defaults=[None, -1, ""])
 
     def __init__(self, code: str, timeout=120):
         self.code = code
@@ -54,8 +58,22 @@ class TikzDocument:
     def pdf(self) -> Optional[PdfDocument]:
         return self.compile().pdf
 
-    def compiled_with_errors(self):
+    @property
+    def log(self) -> str:
+        return self.compile().log
+
+    @property
+    def compiled_with_errors(self) -> bool:
         return self.status != 0
+
+    @cached_property
+    def has_content(self) -> bool:
+        """true if we have an image that isn't empty"""
+        return (img:=self.rasterize()) is not None and img.getcolors(1) is None
+
+    @classmethod
+    def set_engines(cls, engines: Union[str, list]):
+        cls.engines = [engines] if isinstance(engines, str) else engines
 
     @cache
     def compile(self) -> "Output":
@@ -63,30 +81,57 @@ class TikzDocument:
         with TemporaryDirectory() as tmpdirname:
             with NamedTemporaryFile(dir=tmpdirname, buffering=0) as tmpfile:
                 codelines = self.code.split("\n")
-                codelines.insert(1, r"\thispagestyle{empty}") # make sure we don't have page numbers in compiled pdf (for cropping)
+                codelines.insert(1, r"\pagestyle{empty}") # make sure we don't have page numbers in compiled pdf (for cropping)
                 tmpfile.write("\n".join(codelines).encode())
 
                 try:
                     # compile
+                    errorln, tmppdf, outpdf = 0, f"{tmpfile.name}.pdf", join(tmpdirname, "tikz.pdf")
+
+                    def try_save_last_page():
+                        if isfile(tmppdf):
+                            doc = fitz.open(tmppdf) # type: ignore
+                            doc.select([len(doc)-1])
+                            doc.save(outpdf)
+
                     for engine in self.engines:
-                        kwargs = dict(stdout=DEVNULL, stderr=DEVNULL, timeout=self.timeout, cwd=tmpdirname)
-                        output['status'] = run([engine, "-interaction=nonstopmode", tmpfile.name], **kwargs).returncode
-                        if output['status'] == 0:
+                        try:
+                            check_output(
+                                cwd=tmpdirname,
+                                timeout=self.timeout,
+                                stderr=DEVNULL,
+                                env=environ | dict(max_print_line="1000"), # improve formatting of log
+                                args=["latexmk", "-f", "-norc", "-file-line-error", "-interaction=nonstopmode", f"-{engine}", tmpfile.name]
+                            )
+                        except (CalledProcessError, TimeoutExpired) as proc:
+                            log = proc.output.decode(errors="ignore")
+                            error = search(rf'^{escape(tmpfile.name)}:(\d+):.+$', log, MULTILINE)
+                            # only update status and log if first error occurs later than in previous engine
+                            if (linenr:=int(error.group(1)) if error else 0) > errorln:
+                                errorln = linenr
+                                output.update(status=getattr(proc, 'returncode', -1), log=log)
+                                try_save_last_page()
+                        else:
+                            output.update(status=0, log='')
+                            try_save_last_page()
                             break
 
                     # crop
-                    pdfname, cropname = f"{tmpfile.name}.pdf", f"{tmpfile.name}.crop"
-                    crop(["-c", "gb", "-p", "0", "-g", "1", "-o", cropname, pdfname], quiet=True)
-                    if isfile(cropname):
-                        with open(cropname, "rb") as pdf:
+                    croppdf = f"{tmpfile.name}.crop"
+                    crop(["-gsf", "-c", "gb", "-p", "0", "-o", croppdf, outpdf], quiet=True)
+                    if isfile(croppdf):
+                        with open(croppdf, "rb") as pdf:
                             output['pdf'] = PdfDocument(pdf.read())
 
-                except (FileNotFoundError, NameError):
-                    logger.error("Missing dependencies: Consult the project page for help!")
-                except (TimeoutExpired, RuntimeError): #timeout error during compiling, pdf error during cropping
+                except (FileNotFoundError, NameError) as e:
+                    logger.error("Missing dependencies: " + (
+                        "Install this project with the [pdf] feature name!" if isinstance(e, NameError)
+                        else "Did you install TeX Live?"
+                    ))
+                except RuntimeError: # pdf error during cropping
                     pass
 
-        if output.get("status", -1) == 0 and not output.get("pdf", None):
+        if output.get("status") == 0 and not output.get("pdf", None):
             logger.warning("Could compile document but something seems to have gone wrong during cropping!")
 
         return self.Output(**output)
@@ -117,16 +162,17 @@ class TikzGenerator:
         self,
         model,
         tokenizer,
-        temperature=0.2, # generation parameters based on results from codeparrot
-        top_p=0.95,
-        num_beams=1,
-        stream=False,
-        expand_to_square=False,
+        temperature: float = 0.2, # based on "a systematic evaluation of large language models of code"
+        top_p: float = 0.95,
+        top_k: int = 0,
+        stream: bool = False,
+        expand_to_square: bool = False,
+        clean_up_output: bool = True,
     ):
-        self.enc_dec = model.config.is_encoder_decoder
         self.expand_to_square = expand_to_square
+        self.clean_up_output = clean_up_output
         self.processor = getattr(tokenizer, "image", None)
-        self.pipeline = (T2TGP if self.enc_dec else TGP)(
+        self.pipeline = TGP(
             model=model,
             tokenizer=getattr(tokenizer, "text", tokenizer),
             device=current_device() if has_cuda() else -1,
@@ -136,29 +182,52 @@ class TikzGenerator:
         self.default_kwargs = dict(
             temperature=temperature,
             top_p=top_p,
-            num_beams=num_beams,
+            top_k=top_k,
             num_return_sequences=1,
             max_length=self.pipeline.tokenizer.model_max_length, # type: ignore
             do_sample=True,
-            clean_up_tokenization_spaces=True,
-            #remove_invalid_values=True,
             return_full_text=False,
             streamer=TextStreamer(self.pipeline.tokenizer, # type: ignore
                 skip_prompt=True,
-                clean_up_tokenization_spaces=True,
                 skip_special_tokens=True
             ),
         )
 
-        if self.enc_dec:
-            self.default_kwargs.pop("return_full_text")
         if not stream:
             self.default_kwargs.pop("streamer")
 
-    def generate(self, caption: str, image: Optional[Union[Image.Image, str]]=None, **gen_kwargs):
+    @property
+    def is_multimodal(self):
+        return self.processor is not None
+
+    def _run_pipeline(self, prompt: str, **kwargs):
+        tokenizer = self.pipeline.tokenizer
+        text = self.pipeline(prompt, **kwargs)[0]["generated_text"] # type: ignore
+
+        if self.clean_up_output:
+            for token in reversed(tokenizer.tokenize(prompt)): # type: ignore
+                # remove leading characters because skip_special_tokens in pipeline
+                # adds unwanted prefix spaces if prompt ends with a special tokens
+                if text and token in tokenizer.all_special_tokens: # type: ignore
+                    assert text[0].isspace() # type: ignore
+                    text = text[1:]
+                else:
+                    break
+
+            # occasionally observed artifacts
+            artifacts = {
+                r'\bamsop\b': 'amsopn'
+            }
+            for artifact, replacement in artifacts.items():
+                text = sub(artifact, replacement, text) # type: ignore
+
+        return text
+
+    def generate(self, caption: str, snippet: str = "", image: Optional[Union[Image.Image, str]] = None, **gen_kwargs):
         """
         Generate TikZ for a given caption.
             caption: the caption
+            snippet: already existing code snippet to complete
             image: optional input fed into CLIP, defaults to the caption (can be a Pillow Image, a URI to an image, or a caption)
             gen_kwargs: additional generation kwargs (potentially overriding the default ones)
         """
@@ -183,17 +252,16 @@ class TikzGenerator:
         if tokenizer.add_bos_token: # type: ignore
             # unexpectedly pipeline doesn't do this automatically...
             caption = tokenizer.bos_token + caption # type: ignore
-        if not self.enc_dec:
-            # we add a sep token for decoder only models
-            caption = caption + tokenizer.sep_token # type: ignore
+        # we add a sep token for decoder only models
+        caption = caption + tokenizer.sep_token # type: ignore
 
         # Suppress warning about using the pipeline sequentially (we are doing that on purpose!)
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
-            return TikzDocument(self.pipeline( # type: ignore
-                caption,
+            return TikzDocument(snippet + self._run_pipeline( # type: ignore
+                prompt=caption + snippet,
                 **(self.default_kwargs | gen_kwargs),
-            )[0]["generated_text"].strip()) # type: ignore
+            ))
 
     def __call__(self, *args, **kwargs):
         return self.generate(*args, **kwargs)
